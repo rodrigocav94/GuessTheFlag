3f90517ad7373ca3da8b251ca1c45b6488e0b344
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-07 20:50:18 -0300

    Customized flag buttons

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -12,11 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        configureButtonLayout()
+        askQuestion()
+    }
+    
+    func configureButtonLayout() {
+        for button in [button1, button2, button3] {
+            button?.layer.borderWidth = 1
+            button?.layer.borderColor = UIColor.lightGray.cgColor
+        }
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
-
 }
-
