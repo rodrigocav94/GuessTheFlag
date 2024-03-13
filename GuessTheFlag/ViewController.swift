c8ebc3ad55030a3d3fbf2122a61e14e626218d1e
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-13 02:29:43 -0300

    Added IBAction for flag buttons and an alert.

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -41,5 +41,22 @@ class ViewController: UIViewController {
         
         title = countries[correctAnswer].uppercased()
     }
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+    }
 }
