629a73b39ca2f1c8c1058c3fd6f6c56f89546144
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-13 03:18:04 -0300

    Added player score and current/total rounds in the navigation bar Modified alert for showing a different message when it's the final round Letting the user know what country does the flag they tapped belong to in case they take an incorrect guess

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var currentRound = 0
+    let totalRounds = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +36,41 @@ class ViewController: UIViewController {
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        if currentRound >= 10 {
+            currentRound = 0
+            score = 0
+        } else {
+            currentRound += 1
+        }
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+        updateNavBarItems()
+    }
+    
+    func updateNavBarItems() {
+        attachLabelButtonToNavBar(
+            label: "Round: \(currentRound)/\(totalRounds)",
+            toTheLeft: true
+        )
+        
+        attachLabelButtonToNavBar(
+            label: "Score: \(score)",
+            toTheLeft: false
+        )
+    }
+    func attachLabelButtonToNavBar(label: String, toTheLeft: Bool) {
+        let labelView = UILabel()
+        labelView.text = label
+        let labelButton = UIBarButtonItem(customView: labelView)
+        if toTheLeft {
+            navigationItem.leftBarButtonItem = labelButton
+        } else {
+            navigationItem.rightBarButtonItem = labelButton
+        }
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -49,12 +80,18 @@ class ViewController: UIViewController {
             title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong! That’s the flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is: \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        updateNavBarItems()
+        
+        let didFinishAllRounds = currentRound == totalRounds
+        var message = "Your\(didFinishAllRounds ? " final" : "") score is: \(score)"
+        var alertActionLabel = didFinishAllRounds ? "Play again" : "Continue"
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: alertActionLabel, style: .default, handler: askQuestion))
         
         present(ac, animated: true)
         
